63a8e6486d33d85a5eefb9d507c909bb3aa7abe9
petryashov <petryashov@skbkontur.ru>
2018-10-10 23:35:17 +0500

    Revert "remove checkIfRectangleMatrix"
    This reverts commit 9cb8e120abea905ffaabc2ccbc034bf1323b7232.

--- warmup.js
+++ warmup.js
@@ -94,17 +94,30 @@ function matrixProblem(matrix) {
         throw new TypeError('matrix should be 2d array');
     }
 
-    const n = matrix.length;
-
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < matrix.length; i++) {
         if (!(matrix[i] instanceof Array)) {
             throw new TypeError('matrix should be 2d array');
         }
     }
 
+    if (!checkIfRectangleMatrix(matrix)) {
+        throw new TypeError('matrix should be rectangle matrix');
+    }
+
     return matrix[0].map((col, i) => matrix.map(row => row[i]));
 }
 
+function checkIfRectangleMatrix(matrix) {
+    const m = matrix[0].length;
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i].length !== m) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 /**
  * Переводит число в другую систему счисления
