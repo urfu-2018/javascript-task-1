c4ac618446211baf593cddab5f6d785877df4500
Leoltron <leoole5@gmail.com>
2018-10-10 16:41:40 +0500

    First version

--- warmup.js
+++ warmup.js
@@ -1,6 +1,106 @@
 'use strict';
 
 /**
+ * Проверяет тип аргумента
+ * @param {Object} arg Аргумент
+ * @param {String} argName Ммя аргумента
+ * @param {String} expectedTypeName Имя ожидаемого типа
+ * @throws {TypeError} Если тип другой или тип аргументов неверный
+ */
+function checkType(arg, argName, expectedTypeName) {
+    checkTypeUnsafe(argName, 'arg', 'string');
+    checkTypeUnsafe(expectedTypeName, 'expectedTypeName', 'string');
+    checkTypeUnsafe(arg, argName, expectedTypeName);
+}
+
+/**
+ * Проверяет тип аргумента
+ * @param {Object} arg Аргумент
+ * @param {String} argName Ммя аргумента
+ * @param {String} expectedTypeName Имя ожидаемого типа
+ * @throws {TypeError} Если тип другой
+ */
+function checkTypeUnsafe(arg, argName, expectedTypeName) {
+    if (typeof arg !== expectedTypeName) {
+        throw new TypeError(`Expected ${argName} to be a ${expectedTypeName},
+         got ${typeof arg}`);
+    }
+}
+
+/**
+ * Проверяет, что число целое
+ * @param {Number} arg Число
+ * @param {String} argName Название числа
+ * @throws {TypeError} Если arg не число или не целое
+ */
+function checkInteger(arg, argName) {
+    checkType(arg, argName, 'number');
+    checkType(argName, 'argName', 'string');
+    if (arg % 1 !== 0) {
+        throw new TypeError(argName + ' must be integer');
+    }
+}
+
+/**
+ * Проверяет, что число не больше заданного
+ * @param {Number} value Проверяемое значение
+ * @param {Number} max Максимальное допустимое значение
+ * @param {String} valueName Имя проверяемого числа
+ * @throws {RangeError} Если value больше
+ */
+function checkMaxUnsafe(value, max, valueName) {
+    if (value > max) {
+        throw new RangeError(`${valueName} (${value}) must be less than or equal to ${max}`);
+    }
+}
+
+/**
+ * Проверяет, что число не меньше заданного
+ * @param {Number} value Проверяемое значение
+ * @param {Number} min Минимальное допустимое значение
+ * @param {String} valueName Имя проверяемого числа
+ * @throws {RangeError} Если value меньше
+ */
+function checkMinUnsafe(value, min, valueName) {
+    if (min > value) {
+        throw new RangeError(`${valueName} (${value}) must be greater than or equal to ${min}`);
+    }
+}
+
+/**
+ * Проверяет, что число соответствует диапазону
+ * @param {Number} min Минимальное допустимое значение
+ * @param {Number} value Проверяемое значение
+ * @param {Number} max Максимальное допустимое значение
+ * @param {String} valueName Имя проверяемого числа
+ * @throws {RangeError} Если value не входит в диапазон
+ */
+function checkRangeUnsafe(min, value, max, valueName) {
+    checkMinUnsafe(value, min, valueName);
+    checkMaxUnsafe(value, max, valueName);
+}
+
+/**
+ * Проверяет, что число соответствует диапазону
+ * @param {Number} min Минимальное допустимое значение
+ * @param {Number} value Проверяемое значение
+ * @param {Number} max Максимальное допустимое значение
+ * @param {String} valueName Имя проверяемого числа
+ * @throws {TypeError} Если тип аргументов неверный
+ * @throws {RangeError} Если min > max или value не входит в диапазон
+ */
+function checkRange(min, value, max, valueName = 'value') {
+    checkType(min, 'min', 'number');
+    checkType(value, 'value', 'number');
+    checkType(max, 'max', 'number');
+    checkType(valueName, 'valueName', 'string');
+    if (min > max) {
+        throw new RangeError(`min must be less or equal than max, got min (${min}) > max (${max})`);
+    }
+    checkRangeUnsafe(min, value, max, valueName);
+}
+
+/**
  * Складывает два целых числа
  * @param {Number} a Первое целое
  * @param {Number} b Второе целое
@@ -8,7 +108,10 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    // Ваше решение
+    checkInteger(a, 'a');
+    checkInteger(b, 'b');
+
+    return a + b;
 }
 
 /**
@@ -19,7 +122,10 @@ function abProblem(a, b) {
  * @returns {Number} Век, полученный из года
  */
 function centuryByYearProblem(year) {
-    // Ваше решение
+    checkInteger(year, 'year');
+    checkRange(0, year, Number.MAX_VALUE, 'year');
+
+    return Math.floor(year / 100) + 1;
 }
 
 /**
@@ -30,7 +136,13 @@ function centuryByYearProblem(year) {
  * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
  */
 function colorsProblem(hexColor) {
-    // Ваше решение
+    checkType(hexColor, 'hexColor', 'string');
+    checkRange(7, hexColor.length, 7, 'hexColor string length');
+    const red = parseInt(hexColor.slice(1, 3), 16);
+    const green = parseInt(hexColor.slice(3, 5), 16);
+    const blue = parseInt(hexColor.slice(5, 7), 16);
+
+    return `(${red}, ${green}, ${blue})`;
 }
 
 /**
@@ -41,7 +153,50 @@ function colorsProblem(hexColor) {
  * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
  */
 function fibonacciProblem(n) {
-    // Ваше решение
+    checkInteger(n, 'n');
+    checkRange(1, n, Number.MAX_VALUE, 'n');
+
+    return n === 1 || n === 2 ? 1 : fibonacciProblem(n - 1) + fibonacciProblem(n - 2);
+}
+
+/**
+ * Проверяет, что array - двумерный массив
+ * @param {Object} array Проверяемая переменная
+ * @param {String} arrayName Имя проверяемой переменной
+ * @throws [TypeError} Если array - не двумерный массив
+ */
+function checkTwoDimArray(array, arrayName) {
+    if (!Array.isArray(array)) {
+        throw new TypeError(`${arrayName} is not an array`);
+    }
+    const notTwoDimArrayMessage = `Array ${arrayName} is not a two-dimensional array`;
+    if (array.length === 0) {
+        throw new TypeError(notTwoDimArrayMessage);
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (!Array.isArray(array[i])) {
+            throw new TypeError(notTwoDimArrayMessage);
+        }
+    }
+}
+
+/**
+ * Возвращает размеры двумерного массива
+ * @param {(Any[])[]} array двумерный массив
+ * @throws {TypeError} Если array - не двумерный массив или размеры варьируются
+ * @returns {Number[]} Размеры массива
+ */
+function getTwoDimArraySize(array) {
+    checkTwoDimArray(array, 'array');
+    const size1 = array.length;
+    const size2 = array[0].length;
+    for (let i = 1; i < size1; i++) {
+        if (array[i].length !== size2) {
+            throw new TypeError('Second size must not vary');
+        }
+    }
+
+    return [size1, size2];
 }
 
 /**
@@ -51,7 +206,18 @@ function fibonacciProblem(n) {
  * @returns {(Any[])[]} Транспонированная матрица размера NxM
  */
 function matrixProblem(matrix) {
-    // Ваше решение
+    checkTwoDimArray(matrix, 'matrix');
+    const matrixSize = getTwoDimArraySize(matrix);
+    const resultMatrix = [];
+
+    for (let i = 0; i < matrixSize[1]; i++) {
+        resultMatrix[i] = [];
+        for (let j = 0; j < matrixSize[0]; j++) {
+            resultMatrix[i][j] = matrix[j][i];
+        }
+    }
+
+    return resultMatrix;
 }
 
 /**
@@ -63,7 +229,11 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    // Ваше решение
+    checkInteger(n, 'n');
+    checkInteger(targetNs, 'targetNs');
+    checkRange(2, targetNs, 36, 'targetNs');
+
+    return n.toString(targetNs);
 }
 
 /**
@@ -72,7 +242,7 @@ function numberSystemProblem(n, targetNs) {
  * @returns {Boolean} Если соответствует формату, то true, а иначе false
  */
 function phoneProblem(phoneNumber) {
-    // Ваше решение
+    return phoneNumber.match(/^8-800-\d\d\d-\d\d-\d\d$/) !== null;
 }
 
 /**
@@ -82,7 +252,10 @@ function phoneProblem(phoneNumber) {
  * @returns {Number} Количество улыбающихся смайликов в строке
  */
 function smilesProblem(text) {
-    // Ваше решение
+    checkType(text, 'text', 'string');
+    const searchResult = text.match(/(\(-:|:-\))/g);
+
+    return searchResult === null ? 0 : searchResult.length;
 }
 
 /**
@@ -92,7 +265,21 @@ function smilesProblem(text) {
  * @returns {'x' | 'o' | 'draw'} Результат игры
  */
 function ticTacToeProblem(field) {
-    // Ваше решение
+    for (let i = 0; i < 3; i++) {
+        if (field[i][0] === field[i][1] && field[i][1] === field[i][2]) {
+            return field[i][0];
+        }
+
+        if (field[0][i] === field[1][i] && field[1][i] === field[2][i]) {
+            return field[0][i];
+        }
+    }
+
+    if (field[0][0] === field[1][1] && field[1][1] === field[2][2]) {
+        return field[0][0];
+    }
+
+    return 'draw';
 }
 
 module.exports = {
