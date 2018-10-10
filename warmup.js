818688230fe222e49d05ed14f3ad8a0fc080d82c
Nikita Lebedev <acke208@yandex.ru>
2018-10-10 23:32:35 +0500

    кидаю рэндж еррор если не интежер фибоначчи

--- warmup.js
+++ warmup.js
@@ -62,11 +62,11 @@ function colorsProblem(hexColor) {
  * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
  */
 function fibonacciProblem(n) {
-    if (typeof n !== 'number' || !Number.isInteger(n)) {
+    if (typeof n !== 'number') {
         throw new TypeError();
     }
 
-    if (n <= 0) {
+    if (n <= 0 || !Number.isInteger(n)) {
         throw new RangeError();
     }
 
