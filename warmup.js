a8e1c31e3f13c593dbe683c06d7e7317d15db771
Arseniy Garelyshev <Ars.garelyshev@yandex.ru>
2018-10-10 19:00:36 +0500

    fixed transposer not working sometimes

--- warmup.js
+++ warmup.js
@@ -114,7 +114,7 @@ function matrixProblem(matrix) {
         }
     }
 
-    return matrix.map((col, i) => matrix.map(row => row[i]));
+    return matrix[0].map((col, i) => matrix.map(row => row[i]));
 }
 
 /**
