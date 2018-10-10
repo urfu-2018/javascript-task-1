cbf9f8f1bf9d39862b958a242973016676b1e44d
Aleksey <AllEXEcuted>
2018-10-11 02:29:00 +0500

    test

--- warmup.js
+++ warmup.js
@@ -106,7 +106,7 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    if ([n, targetNs].some(e=>typeof(e) !== 'number')) {
+    if ([n, targetNs].some(e=>typeof(e) !== 'number') || !Number.isInteger(targetNs))) {
         throw new TypeError('');
     } else if (targetNs < 2 || targetNs > 36) {
         throw new RangeError('');
