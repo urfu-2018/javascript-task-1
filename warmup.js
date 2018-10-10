765d4deb679a8d49f77f9040c1c39f7a1f5ac633
Nikita Lebedev <acke208@yandex.ru>
2018-10-10 23:10:30 +0500

    Попытка не пытка 3

--- warmup.js
+++ warmup.js
@@ -8,7 +8,10 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    if (isNaN(a) || isNaN(b) || !Number.isInteger(a) || !Number.isInteger(b)) {
+    if (typeof a !== 'number' ||
+        typeof b !== 'number' ||
+        !Number.isInteger(a) ||
+        !Number.isInteger(b)) {
         throw new TypeError();
     }
 
