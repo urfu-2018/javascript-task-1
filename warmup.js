b491794b2bd58c117712371589d9cfab97902b44
Scvairy <scvairy@gmail.com>
2018-10-11 00:43:25 +0500

    Return some thing

--- warmup.js
+++ warmup.js
@@ -132,6 +132,10 @@ function numberSystemProblem(n, targetNs) {
  * @returns {Boolean} Если соответствует формату, то true, а иначе false
  */
 function phoneProblem(phoneNumber) {
+    if (typeof phoneNumber !== 'string') {
+        throw new TypeError();
+    }
+
     return /^8-800-\d{3}(-\d{2}){2}$/.test(phoneNumber);
 }
 
