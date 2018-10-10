5fabbf401ebd82aaf60d82ca68db51ce414601e2
Nikita Lebedev <acke208@yandex.ru>
2018-10-11 01:09:06 +0500

    Вернул

--- warmup.js
+++ warmup.js
@@ -8,10 +8,7 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    if (typeof a !== 'number' ||
-        typeof b !== 'number' ||
-        !Number.isInteger(a) ||
-        !Number.isInteger(b)) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
         throw new TypeError();
     }
 
