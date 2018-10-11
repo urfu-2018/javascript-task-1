af8a1431928722a0f34f3d874071718fb06dd759
Arseniy Garelyshev <Ars.garelyshev@yandex.ru>
2018-10-11 10:33:55 +0500

    NaN checks in the abproblem

--- warmup.js
+++ warmup.js
@@ -8,7 +8,7 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
+    if (typeof a !== 'number' || typeof b !== 'number' || Number.isNaN(a) || Number.isNaN(b)) {
         throw new TypeError('Wrong argument type, expected integer.');
     }
 
