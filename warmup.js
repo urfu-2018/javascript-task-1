29f72dd65f9fbbbeb1a5f9b5181acfd36e01b651
Olga Zhukova <zhukova.o.m@yandex.ru>
2018-10-11 21:40:18 +0500

    что то меняется

--- warmup.js
+++ warmup.js
@@ -23,10 +23,10 @@ function abProblem(a, b) {
  * @returns {Number} Век, полученный из года
  */
 function centuryByYearProblem(year) {
-    if (!isNumber(year) || !Number.isInteger(year)) {
+    if (!Number.isInteger(year)) {
         throw new TypeError();
     }
-    if (year < 0) {
+    if (year < 1) {
         throw new RangeError();
     }
     let century = Math.floor(year / 100);
