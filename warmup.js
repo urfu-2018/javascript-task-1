5bc9869a408f12fb89b2d213cc0543c05180af7a
luckyboy66 <luckyboy66@mail.ru>
2018-10-11 18:54:14 +0500

    check targetNs on int

--- warmup.js
+++ warmup.js
@@ -118,7 +118,7 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    if (!Number.isInteger(n) || !Number.isInteger(targetNs)) {
+    if (!Number.isInteger(n) || !Number.isInteger(targetNs) || Math.floor(targetNs) !== targetNs) {
         throw new TypeError();
     } else if (targetNs < 2 || targetNs > 36) {
         throw new RangeError();
