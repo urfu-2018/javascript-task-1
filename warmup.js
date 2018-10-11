4c148336aad18e65c3cc75e616fa8e4546d2c160
maxticorn <letunovskeymax@gmail.com>
2018-10-11 23:38:50 +0500

    maybe this (try two)

--- warmup.js
+++ warmup.js
@@ -120,7 +120,7 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    if (!Number.isInteger(n) || !Number.isInteger(targetNs)) {
+    if (typeof n != 'number' || !Number.isInteger(targetNs)) {
         throw new TypeError();
     }
     if (targetNs < 2 || targetNs > 36) {
