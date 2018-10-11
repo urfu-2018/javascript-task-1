a8b8629f5ed4da47bbda6bfd25311f00c2f84c60
maxticorn <letunovskeymax@gmail.com>
2018-10-12 00:56:13 +0500

    oh shit rollback

--- warmup.js
+++ warmup.js
@@ -75,8 +75,9 @@ function fibonacciProblem(n) {
     let a = 1;
     let b = 1;
     for (let i = 3; i <= n; i++) {
-        a += b;
-        b = a - b;
+        const c = a + b;
+        a = b;
+        b = c;
     }
 
     return b;
