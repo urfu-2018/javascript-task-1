ee8c67257cc1fad1407756c42098bdc9d084e2e1
luckyboy66 <luckyboy66@mail.ru>
2018-10-11 18:25:30 +0500

    try to fix hex

--- warmup.js
+++ warmup.js
@@ -43,7 +43,7 @@ function centuryByYearProblem(year) {
  * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
  */
 function colorsProblem(hexColor) {
-    if (typeof hexColor !== 'string') {
+    if (typeof hexColor !== 'string' || hexColor.length !== 7) {
         throw new TypeError();
     }
 
