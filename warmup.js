89e2e2a5267b8ca3135c2e0f19fa5b66bb38ee2e
maxticorn <letunovskeymax@gmail.com>
2018-10-11 23:33:51 +0500

    maybe this

--- warmup.js
+++ warmup.js
@@ -8,7 +8,7 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
         throw new TypeError();
     }
 
