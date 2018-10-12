bfbe47544a87103bb5a40ec0e49c3ffc24766b8e
volkov.da <volkov.da@skbkontur.ru>
2018-10-12 12:31:29 +0500

    ну что, попробуем(9)

--- warmup.js
+++ warmup.js
@@ -62,7 +62,7 @@ function colorsProblem(hexColor) {
  * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
  */
 function fibonacciProblem(n) {
-    if (Number.isInteger(n)) {
+    if (!Number.isInteger(n)) {
         throw new TypeError('n is not a number');
     }
     if (n <= 0) {
