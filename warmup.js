cb4fea7d8d106ef7b54ba87daf310b4b3d2916aa
Leoltron <leoole5@gmail.com>
2018-10-12 13:58:28 +0500

    More checks for Tic-Tac-Toe

--- warmup.js
+++ warmup.js
@@ -312,20 +312,20 @@ function smilesProblem(text) {
  */
 function ticTacToeProblem(field) {
     for (let i = 0; i < 3; i++) {
-        if (field[i][0] === field[i][1] && field[i][1] === field[i][2]) {
+        if (field[i][0] && field[i][0] === field[i][1] && field[i][1] === field[i][2]) {
             return field[i][0];
         }
 
-        if (field[0][i] === field[1][i] && field[1][i] === field[2][i]) {
+        if (field[0][i] && field[0][i] === field[1][i] && field[1][i] === field[2][i]) {
             return field[0][i];
         }
     }
 
-    if (field[0][0] === field[1][1] && field[1][1] === field[2][2]) {
+    if (field[0][0] && field[0][0] === field[1][1] && field[1][1] === field[2][2]) {
         return field[1][1];
     }
 
-    if (field[0][2] === field[1][1] && field[1][1] === field[2][0]) {
+    if (field[0][2] && field[0][2] === field[1][1] && field[1][1] === field[2][0]) {
         return field[1][1];
     }
 
