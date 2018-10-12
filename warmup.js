f5de3a865c09a3a774d1fef389dfe31943adee64
ZeroNetto <Zero.Netto.o@gmail.com>
2018-10-12 20:16:37 +0500

    Update warmup.js

--- warmup.js
+++ warmup.js
@@ -83,7 +83,7 @@ function fibonacciProblem(n) {
  * @returns {(Any[])[]} Транспонированная матрица размера NxM
  */
 function matrixProblem(matrix) {
-    if (matrix.length <= 0) {
+    if (!Array.isArray(matrix) || matrix.length <= 0) {
         throw new TypeError();
     }
     for (let i = 0; i < matrix.length; i++) {
