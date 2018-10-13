3dcf9b28038c8a5eb6d3c67c2a7b6f9b060b9313
Ollisteka <zhukova.o.m@yandex.ru>
2018-10-13 19:05:11 +0500

    Неужели???

--- warmup.js
+++ warmup.js
@@ -64,8 +64,8 @@ function colorsProblem(hexColor) {
     }
     hexColor = hexColor.substring(1);
     let r = parseInt(hexColor[0] + hexColor[1], 16);
-    let g = parseInt(hexColor[1] + hexColor[2], 16);
-    let b = parseInt(hexColor[3] + hexColor[4], 16);
+    let g = parseInt(hexColor[2] + hexColor[3], 16);
+    let b = parseInt(hexColor[4] + hexColor[5], 16);
 
     return `(${r}, ${g}, ${b})`;
 }
