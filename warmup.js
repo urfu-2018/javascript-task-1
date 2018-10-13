f3a4b0a878fa86428ce1011a76324376cf461340
Nerzull3 <dima3.98@mail.ru>
2018-10-13 20:51:22 +0500

    1st solution with tests

--- warmup.js
+++ warmup.js
@@ -1,98 +1,109 @@
 'use strict';
 
-/**
- * Складывает два целых числа
- * @param {Number} a Первое целое
- * @param {Number} b Второе целое
- * @throws {TypeError} Когда в аргументы переданы не числа
- * @returns {Number} Сумма аргументов
- */
 function abProblem(a, b) {
-    // Ваше решение
+    if (typeof(a) != "number" || typeof(b) != "number")
+        throw new TypeError();
+    return a + b;
 }
 
-/**
- * Определяет век по году
- * @param {Number} year Год, целое положительное число
- * @throws {TypeError} Когда в качестве года передано не число
- * @throws {RangeError} Когда год – отрицательное значение
- * @returns {Number} Век, полученный из года
- */
 function centuryByYearProblem(year) {
-    // Ваше решение
+    if (typeof year != "number")
+        throw new TypeError();
+    if (year < 0)
+        throw new RangeError();
+    const century = Math.floor(year / 100);
+    
+    return year % 100 == 0 ? century : century + 1;
 }
 
-/**
- * Переводит цвет из формата HEX в формат RGB
- * @param {String} hexColor Цвет в формате HEX, например, '#FFFFFF'
- * @throws {TypeError} Когда цвет передан не строкой
- * @throws {RangeError} Когда значения цвета выходят за пределы допустимых
- * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
- */
 function colorsProblem(hexColor) {
-    // Ваше решение
+    if (typeof hexColor != "string")
+        throw new TypeError();
+    const regex = /#([A-F\d]{2})([A-F\d]{2})([A-F\d]{2})/;
+    const hexRGB = regex.exec(hexColor);
+    const rgb = [hexRGB[1], hexRGB[2], hexRGB[3]];
+    return "(" + rgb.map((value) => parseInt(value, 16)).join(", ") + ")";
 }
 
-/**
- * Находит n-ое число Фибоначчи
- * @param {Number} n Положение числа в ряде Фибоначчи
- * @throws {TypeError} Когда в качестве положения в ряде передано не число
- * @throws {RangeError} Когда положение в ряде не является целым положительным числом
- * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
- */
 function fibonacciProblem(n) {
-    // Ваше решение
+    if (typeof n != "number")
+        throw new TypeError();
+    if (n <= 0)
+        throw new RangeError();
+    
+    let current = 1;
+    let previous = 1;
+    for (let i = 3; i <= n; i++)
+    {   
+        let sum = current + previous;
+        previous = current;
+        current = sum;
+    }
+
+    return current;
 }
 
-/**
- * Транспонирует матрицу
- * @param {(Any[])[]} matrix Матрица размерности MxN
- * @throws {TypeError} Когда в функцию передаётся не двумерный массив
- * @returns {(Any[])[]} Транспонированная матрица размера NxM
- */
 function matrixProblem(matrix) {
-    // Ваше решение
+    if (!(Array.isArray(matrix) && Array.isArray(matrix[0])))
+        throw new TypeError();
+
+    return Object.keys(matrix[0])
+        .map(colNumber => matrix.map(rowNumber => rowNumber[colNumber]));
 }
 
-/**
- * Переводит число в другую систему счисления
- * @param {Number} n Число для перевода в другую систему счисления
- * @param {Number} targetNs Система счисления, в которую нужно перевести (Число от 2 до 36)
- * @throws {TypeError} Когда переданы аргументы некорректного типа
- * @throws {RangeError} Когда система счисления выходит за пределы значений [2, 36]
- * @returns {String} Число n в системе счисления targetNs
- */
 function numberSystemProblem(n, targetNs) {
-    // Ваше решение
+    if (typeof n != "number" || typeof targetNs != "number")
+        throw new TypeError();
+    if (targetNs < 2 || targetNs > 32)
+        throw new RangeError();
+
+    let result = "";
+    while (n >= targetNs)
+    {
+        result = n % targetNs + result;
+        n = Math.floor(n / targetNs);
+    }
+
+    return n + result;
 }
 
-/**
- * Проверяет соответствие телефонного номера формату
- * @param {String} phoneNumber Номер телефона в формате '8–800–xxx–xx–xx'
- * @returns {Boolean} Если соответствует формату, то true, а иначе false
- */
 function phoneProblem(phoneNumber) {
-    // Ваше решение
+    if (typeof phoneNumber != "string")
+        throw new TypeError();
+    const regex = /8-800-[0-9]{3}-[0-9]{2}-[0-9]{2}/;
+    return regex.test(phoneNumber);
 }
 
-/**
- * Определяет количество улыбающихся смайликов в строке
- * @param {String} text Строка в которой производится поиск
- * @throws {TypeError} Когда в качестве аргумента передаётся не строка
- * @returns {Number} Количество улыбающихся смайликов в строке
- */
 function smilesProblem(text) {
-    // Ваше решение
+    if (typeof text != "string")
+        throw new TypeError();
+    const regex = /(:-\)|\(-:)/g;
+    const result = text.match(regex);
+    return result != null ? result.length : 0;
 }
 
-/**
- * Определяет победителя в игре "Крестики-нолики"
- * Тестами гарантируются корректные аргументы.
- * @param {(('x' | 'o')[])[]} field Игровое поле 3x3 завершённой игры
- * @returns {'x' | 'o' | 'draw'} Результат игры
- */
 function ticTacToeProblem(field) {
-    // Ваше решение
+    if (hasWinSequence(field, 'x')) return 'x';
+    if (hasWinSequence(field, 'o')) return 'o';
+    return 'draw';
+}
+
+function hasWinSequence(field, mark) {
+    return isLine(field, mark, 0, 0, 1, 0) ||
+    isLine(field, mark, 0, 1, 1, 0) ||
+    isLine(field, mark, 0, 2, 1, 0) ||
+    isLine(field, mark, 0, 0, 0, 1) ||
+    isLine(field, mark, 1, 0, 0, 1) ||
+    isLine(field, mark, 2, 0, 0, 1) ||
+    isLine(field, mark, 0, 0, 1, 1) ||
+    isLine(field, mark, 2, 0, -1, 1);
+}
+
+function isLine(field, mark, x0, y0, dx, dy) {
+    for (let i = 0; i < 3; i++)
+        if (field[y0 + i * dy][x0 + i * dx] != mark)
+            return false;
+    return true;
 }
 
 module.exports = {
