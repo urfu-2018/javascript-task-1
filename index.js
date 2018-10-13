32dc070ccf43a249d123f6665affbb4e6392e4b9
Nerzull3 <dima3.98@mail.ru>
2018-10-13 22:18:20 +0500

    1st solution. 3nd attempt

--- index.js
+++ index.js
@@ -13,28 +13,28 @@ const {
 } = require('./warmup');
 
 // Выведет `2`
-console.info("2 = " + abProblem(1, 1));
+console.info('2 = ' + abProblem(1, 1));
 // console.info(abProblem('hello', 1));
 // console.info(abProblem(1, 'hello'));
 // console.info(abProblem('hello', 'hello'));
 
 // Выведет `21`
-console.info("21st century = " + centuryByYearProblem(2018));
-console.info("10th century = " + centuryByYearProblem(1000));
-console.info("11st century = " + centuryByYearProblem(1001));
-console.info("1st century = " + centuryByYearProblem(1));
+console.info('21st century = ' + centuryByYearProblem(2018));
+console.info('10th century = ' + centuryByYearProblem(1000));
+console.info('11st century = ' + centuryByYearProblem(1001));
+console.info('1st century = ' + centuryByYearProblem(1));
 // console.info(centuryByYearProblem('2018'));
 
-// Выведет "(255, 255, 255)"
-console.info("hex2rgb: (255, 255, 255) = " + colorsProblem('#FFFFFF'));
-console.info("hex2rgb: (128, 0, 128) = " + colorsProblem('#800080'));
-console.info("hex2rgb: (0, 0, 0) = " + colorsProblem('#000000'));
-console.info("hex2rgb: (165, 42, 42) = " + colorsProblem('#A52A2A'));
+// Выведет '(255, 255, 255)'
+console.info('hex2rgb: (255, 255, 255) = ' + colorsProblem('#FFFFFF'));
+console.info('hex2rgb: (128, 0, 128) = ' + colorsProblem('#800080'));
+console.info('hex2rgb: (0, 0, 0) = ' + colorsProblem('#000000'));
+console.info('hex2rgb: (165, 42, 42) = ' + colorsProblem('#A52A2A'));
 
 // Выведет `1`
-console.info("fibonacci: 1st element is 1 = " + fibonacciProblem(1));
-console.info("fibonacci: 6th element is 8 = " + fibonacciProblem(6));
-console.info("fibonacci: 60th element is 1548008755920 = " + fibonacciProblem(60));
+console.info('fibonacci: 1st element is 1 = ' + fibonacciProblem(1));
+console.info('fibonacci: 6th element is 8 = ' + fibonacciProblem(6));
+console.info('fibonacci: 60th element is 1548008755920 = ' + fibonacciProblem(60));
 // console.info(fibonacciProblem('hello'));
 // console.info(fibonacciProblem(-1));
 
@@ -60,30 +60,30 @@ console.info(matrixProblem([
 // console.info(matrixProblem('a'));
 // console.info(matrixProblem(['a']));
 
-// Выведет "1010101"
-console.info("NS: 1010101 = " + numberSystemProblem(85, 2));
-console.info("NS: 50 = " + numberSystemProblem(40, 8));
-console.info("NS: 45 = " + numberSystemProblem(45, 10));
-console.info("NS: 21 = " + numberSystemProblem(65, 32));
-// console.info(numberSystemProblem("65", 2));
-// console.info(numberSystemProblem(65, "1"));
+// Выведет '1010101'
+console.info('NS: 1010101 = ' + numberSystemProblem(85, 2));
+console.info('NS: 50 = ' + numberSystemProblem(40, 8));
+console.info('NS: 45 = ' + numberSystemProblem(45, 10));
+console.info('NS: 21 = ' + numberSystemProblem(65, 32));
+// console.info(numberSystemProblem('65', 2));
+// console.info(numberSystemProblem(65, '1'));
 // console.info(numberSystemProblem(65, 1));
 // console.info(numberSystemProblem(65, 33));
 
 // Выведет `true`
-console.info("true = " + phoneProblem('8-800-333-51-73'));
-console.info("false = " + phoneProblem('8-908-333-51-73'));
-console.info("false = " + phoneProblem('8-800-33-51-73'));
-console.info("false = " + phoneProblem('8-800-asd-51-73'));
+console.info('true = ' + phoneProblem('8-800-333-51-73'));
+console.info('false = ' + phoneProblem('8-908-333-51-73'));
+console.info('false = ' + phoneProblem('8-800-33-51-73'));
+console.info('false = ' + phoneProblem('8-800-asd-51-73'));
 
 // Выведет `2`
-console.info("smiles: 2 = " + smilesProblem(':-) (-:'));
-console.info("smiles: 2 = " + smilesProblem(':-)-:-(-:'));
-console.info("smiles: 0 = " + smilesProblem(':--)(:'));
-console.info("smiles: 1 = " + smilesProblem(':-(-:'));
-// console.info("smiles: 1 = " + smilesProblem(123);
+console.info('smiles: 2 = ' + smilesProblem(':-) (-:'));
+console.info('smiles: 2 = ' + smilesProblem(':-)-:-(-:'));
+console.info('smiles: 0 = ' + smilesProblem(':--)(:'));
+console.info('smiles: 1 = ' + smilesProblem(':-(-:'));
+// console.info('smiles: 1 = ' + smilesProblem(123);
 
-// Выведет "x"
+// Выведет 'x'
 console.info('x = ' + ticTacToeProblem([
     ['x', 'x', 'x'],
     ['o', 'o', 'x'],
