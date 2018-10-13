ce621a2dc5baff7854f17ed790164d4e04111d4e
Татьяна Гладышева <tatiana.gladysheva24@gmail.com>
2018-10-13 23:08:06 +0500

    еще попытка

--- warmup.js
+++ warmup.js
@@ -120,7 +120,7 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    if (typeof(n) !== 'number' || typeof(targetNs) !== 'number') {
+    if (typeof(n) !== 'number' || typeof(targetNs) !== 'number' || !Number.isInteger(targetNs)) {
         throw new TypeError('Аргументы должны быть числами');
     }
     if (targetNs < 2 || targetNs > 36) {
