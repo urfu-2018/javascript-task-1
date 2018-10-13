d1dbae4f936702ce26f84c0a828bcd923ef08108
Татьяна Гладышева <tatiana.gladysheva24@gmail.com>
2018-10-13 23:12:51 +0500

    больше проверок в abProblem

--- warmup.js
+++ warmup.js
@@ -8,7 +8,8 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    if (typeof (a) !== 'number' || typeof (b) !== 'number') {
+    if (typeof (a) !== 'number' || typeof (b) !== 'number' ||
+     !Number.isInteger(a) || !Number.isInteger(b)) {
         throw new TypeError('Аргументы должны быть целыми числами');
     }
 
