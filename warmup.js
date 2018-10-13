4e29961fe5e10ac1b1794e5f2733830b0df3e55c
Монин Константин <33116124+Kuvadorian@users.noreply.github.com>
2018-10-13 22:06:10 +0500

    (10)

--- warmup.js
+++ warmup.js
@@ -175,12 +175,11 @@ function ticTacToeProblem(field) {
     for (let i = 0; i < 3; i++) {
         if (field[i][0] === field[i][1] === field[i][2]) {
             return field[i][0];
-        } else {
-            if (field[0][i] === field[1][i] === field[2][i]) {
-                return field[0][i];
-            }
+        } else if (field[0][i] === field[1][i] === field[2][i]) {
+            return field[0][i];
         }
     }
+
     return 'draw';
 }
 
