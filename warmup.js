9d044816c686a3cf31f6da28fbf86503bdd9cc23
Ollisteka <zhukova.o.m@yandex.ru>
2018-10-13 18:36:18 +0500

    Добавим проверочку

--- warmup.js
+++ warmup.js
@@ -158,6 +158,10 @@ function numberSystemProblem(n, targetNs) {
  * @returns {Boolean} Если соответствует формату, то true, а иначе false
  */
 function phoneProblem(phoneNumber) {
+    if (!isString(phoneNumber)) {
+        throw new TypeError();
+    }
+
     return /^\s*8-800-\d{3}-\d{2}-\d{2}\s*$/.test(phoneNumber);
 }
 
