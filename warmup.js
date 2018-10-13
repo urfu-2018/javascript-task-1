71a561dffc8dcd071cddad8885d574f6848d15a3
Татьяна Гладышева <tatiana.gladysheva24@gmail.com>
2018-10-13 23:29:47 +0500

    следующая попытка

--- warmup.js
+++ warmup.js
@@ -27,7 +27,7 @@ function centuryByYearProblem(year) {
     if (typeof(year) !== 'number' || !Number.isInteger(year)) {
         throw new TypeError('Аргументы должен быть целым числом');
     }
-    if (year < 0) {
+    if (year <= 0) {
         throw new RangeError('Год должен быть положительным значением');
     }
     if (year % 100 === 0) {
@@ -48,8 +48,9 @@ function colorsProblem(hexColor) {
     if (typeof(hexColor) !== 'string') {
         throw new TypeError('Ожидается строка');
     }
-    var re = new RegExp('#[A-Fa-f0-9]{6}');
-    if (!re.test(hexColor) || hexColor.length !== 7) {
+    var re = new RegExp('^#[A-Fa-f0-9]{6}$');
+    var matchRes = hexColor.match(re);
+    if (matchRes === null) {
         throw new RangeError('Ожидается строка');
     }
 
@@ -69,10 +70,10 @@ function colorsProblem(hexColor) {
  * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
  */
 function fibonacciProblem(n) {
-    if (typeof(n) !== 'number' || !Number.isInteger(n)) {
+    if (typeof(n) !== 'number') {
         throw new TypeError('Аргумент должен быть целым числом');
     }
-    if (n <= 0) {
+    if (n <= 0 || !Number.isInteger(n)) {
         throw new RangeError('n должно быть положительным значением');
     }
     var f = 1;
@@ -139,7 +140,7 @@ function numberSystemProblem(n, targetNs) {
 function phoneProblem(phoneNumber) {
     var phoneRegex = new RegExp('8[–-]800[–-][0-9]{3}[–-][0-9]{2}[–-][0-9]{2}');
     if (typeof(phoneNumber) !== 'string') {
-        return false;
+        throw new TypeError('Ожидается строка');
     }
 
     return phoneNumber.length === 15 && phoneRegex.test(phoneNumber);
@@ -170,7 +171,7 @@ function smilesProblem(text) {
 function ticTacToeProblem(field) {
 
     let rowCheck = existsRowWithSameSymbols(field);
-    if (rowCheck !== null) {
+    if (rowCheck !== false) {
         return rowCheck;
     }
     for (let i = 0; i < 3; i++) {
@@ -197,7 +198,7 @@ function existsRowWithSameSymbols(field) {
         }
     }
 
-    return null;
+    return false;
 }
 module.exports = {
     abProblem,
