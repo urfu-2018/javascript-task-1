f2868168bc762c46ad6e31d7f53bb4ae71065117
Egor Naumov <Egor_Nmv@bk.ru>
2018-10-15 01:13:23 +0500

    v1.0

--- warmup.js
+++ warmup.js
@@ -8,7 +8,10 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    // Ваше решение
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        return (a + b);
+    }
+    throw TypeError;
 }
 
 /**
@@ -19,7 +22,10 @@ function abProblem(a, b) {
  * @returns {Number} Век, полученный из года
  */
 function centuryByYearProblem(year) {
-    // Ваше решение
+    if (Number.isInteger(year) > 0) {
+        return (Math.trunc(year / 100) + 1);
+    }
+    throw RangeError;
 }
 
 /**
@@ -30,7 +36,17 @@ function centuryByYearProblem(year) {
  * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
  */
 function colorsProblem(hexColor) {
-    // Ваше решение
+    if (typeof(hexColor) === 'string' && ((hexColor.length) = 7)) {
+        const clearHex = hexColor.substring(1, 7);
+        const r = parseInt(clearHex.substring(0, 2), 16);
+        const g = parseInt(clearHex.substring(2, 4), 16);
+        const b = parseInt(clearHex.substring(4, 6), 16);
+        if ((r, g, b) <= 255 && (r, g, b) >= 0) {
+            return (r + ',' + g + ',' + b);
+        }
+        throw RangeError;
+    }
+    throw TypeError;
 }
 
 /**
@@ -41,7 +57,23 @@ function colorsProblem(hexColor) {
  * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
  */
 function fibonacciProblem(n) {
-    // Ваше решение
+    if (typeof(n) === 'number') {
+        if (Number.isInteger(n) > 0) {
+            if (n <= 2) {
+                return 1;
+            }
+            const fn = 1;
+            const fnmin1 = 0;
+            for (let i = 1; i < n; i++) {
+                fn += fnmin1;
+                fnmin1 = fn - fnmin1;
+            }
+            return fnmin1;
+            // return (fibonacciProblem(n-1)+fibonacciProblem(n-2));
+        }
+        throw RangeError;
+    }
+    throw TypeError;
 }
 
 /**
@@ -51,7 +83,19 @@ function fibonacciProblem(n) {
  * @returns {(Any[])[]} Транспонированная матрица размера NxM
  */
 function matrixProblem(matrix) {
-    // Ваше решение
+    if (Array.isArray(matrix) && Array.isArray(matrix[0])) {
+        const M = matrix.length;
+        const N = matrix[0].length;
+        const transpMatrix = [];
+        for (let i = 0; i < N; i++) {
+            transpMatrix[i] = [];
+            for (let j = 0; j < M; j++) {
+                transpMatrix[i][j] = matrix[j][i] ;
+            }
+        }
+        return transpMatrix;
+    }
+    throw TypeError;
 }
 
 /**
@@ -63,7 +107,22 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    // Ваше решение
+    if (typeof(n) === 'number' && typeof(targetNs) === 'number') {
+        if (targetNs <= 36 || targetNs >= 2) {
+            const transfer = [];
+            const i = 1;
+            const rem = n / targetNs;
+            transfer[0] = n % targetNs;
+            while (rem !== 0) {
+                rem /= targetNs;
+                transfer[i] = rem % targetNs;
+                i++;
+            }
+            return (toString((transfer.reverse()).join()));
+        }
+        throw RangeError;
+    }
+    throw TypeError;
 }
 
 /**
@@ -72,7 +131,21 @@ function numberSystemProblem(n, targetNs) {
  * @returns {Boolean} Если соответствует формату, то true, а иначе false
  */
 function phoneProblem(phoneNumber) {
-    // Ваше решение
+    if (phoneNumber.substring(0, 1) === '8' &&
+    phoneNumber.substring(1, 2) === '–' &&
+    phoneNumber.substring(2, 5) === '800' &&
+    phoneNumber.substring(5, 6) === '–' &&
+    (phoneNumber.substring(6, 9)).length === 3 &&
+    typeof((phoneNumber.substring(6, 9))) === 'number' &&
+    phoneNumber.substring(9, 10) === '–' &&
+    typeof((phoneNumber.substring(10, 12))) === 'number' &&
+    (phoneNumber.substring(10, 12)).length === 2 &&
+    phoneNumber.substring(12, 13) === '–' &&
+    typeof((phoneNumber.substring(13, 15))) === 'number' &&
+    (phoneNumber.substring(13, 15)).length === 2 ) {
+        return true;
+    }
+    return false;
 }
 
 /**
@@ -82,7 +155,21 @@ function phoneProblem(phoneNumber) {
  * @returns {Number} Количество улыбающихся смайликов в строке
  */
 function smilesProblem(text) {
-    // Ваше решение
+    if (typeof(text) === 'string') {
+        const numofSmile = 0;
+        const currentPos = 0;
+        const allSmile = 0;
+        while (true) {
+            numofSmile = text.indexOf('(-:' || ':-)', currentPos);
+            if (numofSmile === -1) {
+                break;
+            }
+            currentPos = numofSmile + 1;
+            allSmile += 1;
+        }
+        return allSmile;
+    }
+    return TypeError;
 }
 
 /**
@@ -92,7 +179,30 @@ function smilesProblem(text) {
  * @returns {'x' | 'o' | 'draw'} Результат игры
  */
 function ticTacToeProblem(field) {
-    // Ваше решение
+    if (Array.isArray(field) && (Array.isArray(field[0])) && field[0][0] === 'x' &&
+    field[0][1] === 'x' && field[0][2] === 'x' ||
+    field[1][0] === 'x' && field[1][1] === 'x' && field[1][2] === 'x' ||
+    field[2][0] === 'x' && field[2][1] === 'x' && field[2][2] === 'x' ||
+    field[0][0] === 'x' && field[1][0] === 'x' && field[2][0] === 'x' ||
+    field[0][1] === 'x' && field[1][1] === 'x' && field[2][1] === 'x' ||
+    field[0][2] === 'x' && field[1][2] === 'x' && field[2][2] === 'x' ||
+    field[0][0] === 'x' && field[1][1] === 'x' && field[2][2] === 'x' ||
+    field[0][2] === 'x' && field[1][1] === 'x' && field[2][0] === 'x') {
+        return 'x';
+    }
+    else if (field[0][0] === 'o' && field[0][1] === 'o' && field[0][2] === 'o' ||
+        field[1][0] === 'o' && field[1][1] === 'o' && field[1][2] === 'o' ||
+        field[2][0] === 'o' && field[2][1] === 'o' && field[2][2] === 'o' ||
+        field[0][0] === 'o' && field[1][0] === 'o' && field[2][0] === 'o' ||
+        field[0][1] === 'o' && field[1][1] === 'o' && field[2][1] === 'o' ||
+        field[0][2] === 'o' && field[1][2] === 'o' && field[2][2] === 'o' ||
+        field[0][0] === 'o' && field[1][1] === 'o' && field[2][2] === 'o' ||
+        field[0][2] === 'o' && field[1][1] === 'o' && field[2][0] === 'o') {
+        return 'o';
+    }
+    else {
+        return 'draw';
+    }
 }
 
 module.exports = {
