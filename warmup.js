da46e5dedd6e8e1667209f1cff0dc364e72cb27f
Корабельникова Татьяна <tk-imi@yandex.ru>
2018-10-14 14:46:16 +0500

    phoneProblem: добавила проверку на строку

--- warmup.js
+++ warmup.js
@@ -126,6 +126,10 @@ function numberSystemProblem(n, targetNs) {
  * @returns {Boolean} Если соответствует формату, то true, а иначе false
  */
 function phoneProblem(phoneNumber) {
+    if (typeof phoneNumber !== 'string') {
+        throw new TypeError('phone number should be a string');
+    }
+
     return /^8-800-\d{3}-\d{2}-\d{2}$/.test(phoneNumber);
 }
 
