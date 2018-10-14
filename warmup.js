585874884e5e5183d76d31393eb2903d7e7fac19
Vladimir <e3250@yandex.ru>
2018-10-14 14:21:41 +0500

    Update warmup.js

--- warmup.js
+++ warmup.js
@@ -111,7 +111,7 @@ function matrixProblem(matrix) {
 }
 
 function checkArray(matrix, len) {
-    if (!Array.isArray(matrix) || matrix.length !== len) {
+    if (matrix.length !== len || !Array.isArray(matrix)) {
         throw new TypeError('Argument must be a 2-dim array');
     }
 }
