884382ae84e6623e54c8029a034d5ac17b6b9f94
Vladimir <e3250@yandex.ru>
2018-10-14 12:57:43 +0500

    Update warmup.js

--- warmup.js
+++ warmup.js
@@ -121,7 +121,7 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    if (!Number.isInteger(n) || !Number.isInteger(targetNs)) {
+    if (typeof(n) !== 'number' || typeof(targetNs) !== 'number' || !Number.isInteger(targetNs)) {
         throw new TypeError('ВВедите цифры!');
     } else if (targetNs < 2 || targetNs > 36 || targetNs % 1 !== 0) {
         throw new RangeError('');
