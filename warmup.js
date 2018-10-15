18376c307a6f0bed349299269d649227d7efd059
Alexander Kartashov <alexander736b@yandex.ru>
2018-10-16 03:39:17 +0500

    Еще немного

--- warmup.js
+++ warmup.js
@@ -186,18 +186,18 @@ function getWinner(field) {
         }
     }
 
-    if (field[0][0] === field[1][1] && field[0][0] === field[2][2]) {
-        if (field[0][0] === 'x') {
-            return 'x';
-        }
-
-        return 'o';
-    }
-
     return 'draw';
 }
 
 function ticTacToeProblem(field) {
+    if (field[0][0] === field[1][1] && field[0][0] === field[2][2]) {
+        return field[0][0];
+    }
+
+    if (field[2][0] === field[1][1] && field[2][0] === field[0][2]) {
+        return field[2][0];
+    }
+
     let winner = getWinner(field);
 
     if (winner === 'draw') {
