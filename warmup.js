ebea0ad6cff42bcc4b64da96c987f6db29d833dc
Afanasev Leonid <leo-6809@mail.ru>
2018-10-15 21:23:57 +0500

    Моё решение задачи 3

--- warmup.js
+++ warmup.js
@@ -183,17 +183,21 @@ function smilesProblem(text) {
  */
 function ticTacToeProblem(field) {
     for (let i = 0; i < field.length; i++) {
-        if (field[i][0] === field[i][1] && field[i][0] === field[i][2])
+        if (field[i][0] === field[i][1] && field[i][0] === field[i][2]) {
             return field[i][0];
+        }
     }
     for (let i = 0; i < field.length; i++) {
-        if (field[0][i] === field[1][i] && field[0][i] === field[2][i])
+        if (field[0][i] === field[1][i] && field[0][i] === field[2][i]) {
             return field[0][i];
+        }
     }
-    if (field[0][0] === field[1][1] && field[0][0] === field[2][2])
+    if (field[0][0] === field[1][1] && field[0][0] === field[2][2]) {
         return field[0][0];
-    if (field[2][0] === field[1][1] && field[2][0] === field[0][2])
+    }
+    if (field[2][0] === field[1][1] && field[2][0] === field[0][2]) {
         return field[2][0];
+    }
 
     return 'draw';
 }
