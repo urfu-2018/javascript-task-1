4447cedd35930edea979dec6c55cbedfa78e6d24
jellymary <marykuzovleva@gmail.com>
2018-10-15 23:08:33 +0500

    fix colorsProblem 2

--- warmup.js
+++ warmup.js
@@ -45,7 +45,7 @@ function colorsProblem(hexColor) {
     if (typeof(hexColor) !== 'string') {
         throw new TypeError();
     }
-    if (/^#[0-9A-Fa-f]{6}$/.test(hexColor)) {
+    if (!(/^#[0-9A-Fa-f]{6}$/).test(hexColor)) {
         throw new RangeError();
     }
     const rgb = Array(3);
