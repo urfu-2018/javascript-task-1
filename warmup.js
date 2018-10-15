680d25aecc7012692ffb570054a70c4e82db3369
SaverAlex <alexeymaruhin@yandex.ru>
2018-10-15 18:02:47 +0500

    Первое решение задачи

--- warmup.js
+++ warmup.js
@@ -7,8 +7,13 @@
  * @throws {TypeError} Когда в аргументы переданы не числа
  * @returns {Number} Сумма аргументов
  */
+
 function abProblem(a, b) {
-    // Ваше решение
+    if (!isNaN(a) && !isNaN(b)) {
+        return a + b;
+    }
+
+    return TypeError;// Succses
 }
 
 /**
@@ -18,8 +23,23 @@ function abProblem(a, b) {
  * @throws {RangeError} Когда год – отрицательное значение
  * @returns {Number} Век, полученный из года
  */
+
 function centuryByYearProblem(year) {
-    // Ваше решение
+    if (isNaN(year)) {
+        return TypeError;
+    }
+    if (year > 0) {
+        if (year % 1000 === 0) {
+            const res = Math.floor(year / 100);
+
+            return res;
+        }
+        const res = Math.floor(year / 100) + 1;
+
+        return res;
+    }
+
+    return RangeError;
 }
 
 /**
@@ -29,8 +49,24 @@ function centuryByYearProblem(year) {
  * @throws {RangeError} Когда значения цвета выходят за пределы допустимых
  * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
  */
+
 function colorsProblem(hexColor) {
-    // Ваше решение
+    if (typeof(hexColor) !== 'string') {
+        return TypeError;
+    }
+    let result = '(';
+    let j = 1;
+    for (let i = 0; i < 3; i++) {
+        const x = parseInt(hexColor.substring(1, 3), 16) + ', ';
+        if (x < 0 || x > 255) {
+            return RangeError;
+        }
+        result = result + x;
+        j = j + 2;
+    }
+    result = result.substring(0, result.length - 2) + ')';
+
+    return result;
 }
 
 /**
@@ -40,8 +76,22 @@ function colorsProblem(hexColor) {
  * @throws {RangeError} Когда положение в ряде не является целым положительным числом
  * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
  */
+
 function fibonacciProblem(n) {
-    // Ваше решение
+    let array = [0, 1];
+    if (isNaN(n)) {
+
+        return TypeError;
+    }
+    if (Number.isInteger(n) && n > 0) {
+        for (let i = 2; i <= n; i++) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+
+        return array[array.length - 1];
+    }
+
+    return RangeError;
 }
 
 /**
@@ -50,8 +100,26 @@ function fibonacciProblem(n) {
  * @throws {TypeError} Когда в функцию передаётся не двумерный массив
  * @returns {(Any[])[]} Транспонированная матрица размера NxM
  */
+
 function matrixProblem(matrix) {
-    // Ваше решение
+    if (!Array.isArray(matrix)) {
+        return TypeError;
+    }
+    let val = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        const arAr = matrix[i];
+        if (!Array.isArray(arAr)) {
+            return TypeError;
+        }
+        for (let j = 0 + val; j < matrix[i].length; j++) {
+            const x = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = x;
+        }
+        val++;
+    }
+
+    return matrix;
 }
 
 /**
@@ -62,8 +130,19 @@ function matrixProblem(matrix) {
  * @throws {RangeError} Когда система счисления выходит за пределы значений [2, 36]
  * @returns {String} Число n в системе счисления targetNs
  */
-function numberSystemProblem(n, targetNs) {
-    // Ваше решение
+
+function numberSystemProblem(n, targetNs) { //  ПОЛНОСТЬЮ ПЕРЕДЕЛАТЬ
+    if (isNaN(n) && isNaN(targetNs)) {
+
+        return TypeError;
+    }
+    if (targetNs >= 2 && targetNs <= 36) {
+        const result = n.toString(targetNs);
+
+        return result;
+    }
+
+    return RangeError;
 }
 
 /**
@@ -71,8 +150,32 @@ function numberSystemProblem(n, targetNs) {
  * @param {String} phoneNumber Номер телефона в формате '8–800–xxx–xx–xx'
  * @returns {Boolean} Если соответствует формату, то true, а иначе false
  */
+
 function phoneProblem(phoneNumber) {
-    // Ваше решение
+    const array = phoneNumber.split('-');
+    console.info(array);
+    if (array[0] !== '8') {
+
+        return false;
+    }
+    if (array[1] !== '800') {
+
+        return false;
+    }
+    if (array[2].length !== 3) {
+
+        return false;
+    }
+    if (array[3].length !== 2) {
+
+        return false;
+    }
+    if (array[4].length !== 2) {
+
+        return false;
+    }
+
+    return true;
 }
 
 /**
@@ -81,8 +184,27 @@ function phoneProblem(phoneNumber) {
  * @throws {TypeError} Когда в качестве аргумента передаётся не строка
  * @returns {Number} Количество улыбающихся смайликов в строке
  */
+
 function smilesProblem(text) {
-    // Ваше решение
+    let num = 0;
+    if (typeof(text) !== 'string') {
+
+        return TypeError;
+    }
+    let skip = 0;
+    let find = '(-:';
+    for (let i = 0; i < 2; i++) {
+        let sign = text.indexOf(find);
+        skip = sign;
+        while (sign !== -1) {
+            num = num + 1;
+            sign = text.indexOf(find, skip + 1);
+            skip = sign;
+        }
+        find = ':-)';
+    }
+
+    return num;
 }
 
 /**
@@ -91,8 +213,50 @@ function smilesProblem(text) {
  * @param {(('x' | 'o')[])[]} field Игровое поле 3x3 завершённой игры
  * @returns {'x' | 'o' | 'draw'} Результат игры
  */
+
 function ticTacToeProblem(field) {
-    // Ваше решение
+    const hz = horizontal(field);
+    if (hz !== -1) {
+        return hz;
+    }
+    const vc = vertical(field);
+    if (vc !== -1) {
+        return vc;
+    }
+    if (field[0][0] === field[1][1] && field[1][1] === field[2][2]) {
+        return field[0][0];
+    }
+    if (field[2][2] === field[1][1] && field[1][1] === field[0][2]) {
+        return field[1][0];
+    }
+}
+
+function horizontal(field) {
+    if (field[0][0] === field[0][1] && field[0][1] === field[0][2]) {
+        return field[0][0];
+    }
+    if (field[1][0] === field[1][1] && field[1][1] === field[1][2]) {
+        return field[1][0];
+    }
+    if (field[2][0] === field[2][1] && field[2][1] === field[2][2]) {
+        return field[2][0];
+    }
+
+    return -1;
+}
+
+function vertical(field) {
+    if (field[0][0] === field[1][0] && field[1][0] === field[2][0]) {
+        return field[0][0];
+    }
+    if (field[0][1] === field[1][1] && field[1][1] === field[2][1]) {
+        return field[1][1];
+    }
+    if (field[0][1] === field[1][2] && field[1][2] === field[2][2]) {
+        return field[2][1];
+    }
+
+    return -1;
 }
 
 module.exports = {
