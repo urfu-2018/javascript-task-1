00705bb6c2407a06a12cbdba8b8f29b2cd1e58fe
VA-Chusovitin <tchsl98@yandex.ru>
2018-10-16 02:57:42 +0500

    Остальное завтра, если успею

--- warmup.js
+++ warmup.js
@@ -114,7 +114,7 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    if (typeof(n) !== 'number' && typeof(targetNs) !== 'number') {
+    if (typeof(n) !== 'number' || typeof(targetNs) !== 'number' || targetNs % 1 !== 0) {
         throw TypeError;
     }
     if (targetNs >= 2 && targetNs <= 36) {
