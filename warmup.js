0a6a2c8d858c5080feadc3555e6d83fb65d9af7f
VA-Chusovitin <tchsl98@yandex.ru>
2018-10-16 18:10:08 +0500

    исправил ошибку

--- warmup.js
+++ warmup.js
@@ -132,7 +132,7 @@ function phoneProblem(phoneNumber) {
     phoneNumber = phoneNumber.replace(/-/g, '');
 
     return phoneNumber.length === 11 && phoneNumber.substr(0, 4) === '8800' &&
-    typeof(phoneNumber.substr(4) === 'number');
+    typeof(phoneNumber.substr(4)) === 'number';
 }
 
 /**
