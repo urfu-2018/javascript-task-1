c903aa04b09d58ae1b57f3c85978790afeabd987
Ivan Smirnov <Van10000@mail.ru>
2018-10-17 01:52:04 +0500

    Wrote all

--- warmup.js
+++ warmup.js
@@ -1,5 +1,34 @@
 'use strict';
 
+function numberIsInteger(number) {
+    return number % 1 === 0;
+}
+
+function range(size, startAt = 0) {
+    return [...Array(size).keys()].map(i => i + startAt);
+}
+
+function isNumber(num) {
+    return Number(num) === num;
+}
+
+function isString(str) {
+    return typeof(str) === 'string' || str instanceof String;
+}
+
+function isArray(arr) {
+    return arr instanceof Array;
+}
+
+function assertIfTypesDoNotMatch(args, types) {
+    for (let i = 0; i < args.length; ++i) {
+        if (!(types[i](args[i]))) {
+            throw new TypeError(`${args[i]} has incorrect type.`);
+        }
+    }
+}
+
+
 /**
  * Складывает два целых числа
  * @param {Number} a Первое целое
@@ -8,7 +37,9 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    // Ваше решение
+    assertIfTypesDoNotMatch([a, b], [isNumber, isNumber]);
+
+    return a + b;
 }
 
 /**
@@ -19,7 +50,11 @@ function abProblem(a, b) {
  * @returns {Number} Век, полученный из года
  */
 function centuryByYearProblem(year) {
-    // Ваше решение
+    assertIfTypesDoNotMatch([year], [isNumber]);
+    if (year >= 0) {
+        return Math.floor((year - 1) / 100) + 1;
+    }
+    throw new RangeError('Year is less than zero');
 }
 
 /**
@@ -30,7 +65,18 @@ function centuryByYearProblem(year) {
  * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
  */
 function colorsProblem(hexColor) {
-    // Ваше решение
+    const HEX_COLOR_RE = /^#[0-9a-fA-F]{6}$/;
+
+    assertIfTypesDoNotMatch([hexColor], [isString]);
+    if (HEX_COLOR_RE.test(hexColor)) {
+        let color = [];
+        for (let i = 1; i < 7; i += 2) {
+            color.push(parseInt(hexColor.substr(i, 2), 16));
+        }
+
+        return `(${color.join(', ')})`;
+    }
+    throw new RangeError(`hexColor ${hexColor} contains not allowed symbols or has invalid length`);
 }
 
 /**
@@ -41,7 +87,19 @@ function colorsProblem(hexColor) {
  * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
  */
 function fibonacciProblem(n) {
-    // Ваше решение
+    assertIfTypesDoNotMatch([n], [isNumber]);
+    if (numberIsInteger(n) && n > 0) {
+        let current = 1;
+        let next = 1;
+        for (let i = 1; i < n; ++i) {
+            let afterNext = current + next;
+            current = next;
+            next = afterNext;
+        }
+
+        return current;
+    }
+    throw new RangeError(`Number should be positive, given ${n}`);
 }
 
 /**
@@ -51,7 +109,23 @@ function fibonacciProblem(n) {
  * @returns {(Any[])[]} Транспонированная матрица размера NxM
  */
 function matrixProblem(matrix) {
-    // Ваше решение
+    assertIfTypesDoNotMatch([matrix], [isArray]);
+    if (!matrix.every(arr => arr instanceof Array && arr.length === matrix[0].length)) {
+        throw new TypeError('Matrix elements are not arrays of same lengths.');
+    }
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let transposed = Array(n)
+        .fill(0)
+        // eslint-disable-next-line no-unused-vars
+        .map(_ => Array(m).fill(0));
+    for (let i = 0; i < m; ++i) {
+        for (let j = 0; j < n; ++j) {
+            transposed[j][i] = matrix[i][j];
+        }
+    }
+
+    return transposed;
 }
 
 /**
@@ -63,7 +137,12 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    // Ваше решение
+    assertIfTypesDoNotMatch([n, targetNs], [isNumber, isNumber]);
+    if (range(35, 2).findIndex(x => x === targetNs) === -1) {
+        throw new RangeError('targetNs is not integet between 2 and 36');
+    }
+
+    return n.toString(targetNs);
 }
 
 /**
@@ -72,7 +151,10 @@ function numberSystemProblem(n, targetNs) {
  * @returns {Boolean} Если соответствует формату, то true, а иначе false
  */
 function phoneProblem(phoneNumber) {
-    // Ваше решение
+    assertIfTypesDoNotMatch([phoneNumber], [isString]);
+    const PHONE_RE = /^8-800-\d{3}-\d{2}-\d{2}$/;
+
+    return PHONE_RE.test(phoneNumber);
 }
 
 /**
@@ -82,7 +164,9 @@ function phoneProblem(phoneNumber) {
  * @returns {Number} Количество улыбающихся смайликов в строке
  */
 function smilesProblem(text) {
-    // Ваше решение
+    assertIfTypesDoNotMatch([text], [isString]);
+
+    return (text.match(/:-\)/g) || []).length;
 }
 
 /**
@@ -91,8 +175,23 @@ function smilesProblem(text) {
  * @param {(('x' | 'o')[])[]} field Игровое поле 3x3 завершённой игры
  * @returns {'x' | 'o' | 'draw'} Результат игры
  */
+// eslint-disable-next-line complexity
 function ticTacToeProblem(field) {
-    // Ваше решение
+    let transposed = matrixProblem(field);
+    let winner;
+    function winnerAtLine(index, game) {
+        return (game[index].every(ch => ch === game[index][0])) ? game[index][0] : undefined;
+    }
+    function winnerAtMainDiagonal(game) {
+        return (game[0][0] === game[1][1] && game[0][0] === game[2][2]) ? game[0][0] : undefined;
+    }
+
+    for (let index = 0; index < 3; ++index) {
+        winner = winner || winnerAtLine(index, field) || winnerAtLine(index, transposed);
+    }
+    winner = winner || winnerAtMainDiagonal(field) || winnerAtMainDiagonal(transposed);
+
+    return winner || 'draw';
 }
 
 module.exports = {
