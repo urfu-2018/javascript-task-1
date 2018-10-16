150ff98013c2ead20b13e8412512341af0c11307
Alexander Kartashov <alexander736b@yandex.ru>
2018-10-16 22:45:07 +0500

    Возможно всеÐ

--- warmup.js
+++ warmup.js
@@ -70,13 +70,13 @@ function fibonacci(n) {
     let a = 1;
     let b = 1;
 
-    for (let i = 2; i <= n; i++) {
+    for (let i = 3; i <= n; i++) {
         let c = a + b;
         a = b;
         b = c;
     }
 
-    return a;
+    return b;
 }
 
 function fibonacciProblem(n) {
