a45b1a40dbf751f62b5ea899708db3d2551d6263
chichi67 <paulmbasela4@gmail.com>
2018-10-16 19:52:09 +0500

    agaiin

--- warmup.js
+++ warmup.js
@@ -9,11 +9,13 @@
  */
 function abProblem(a, b) {
     // Ваше решение
-var sum= a+b;
-var newnum= Number(sum);
-document.write(newnum);
+
+if (Number.isInteger(a) === false || Number.isInteger(b) === false) {
+    throw new TypeError();
 }
-abProblem(400, 200);
+return a + b;
+}
+
 /**
  * Определяет век по году
  * @param {Number} year Год, целое положительное число
@@ -23,12 +25,17 @@ abProblem(400, 200);
  */
 function centuryByYearProblem(year) {
     // Ваше решение
-    if (year>=0){
+    if (Number.isInteger(year) === false) {
+        throw new TypeError();
+    } 
+    else if (year < 0) {
+        throw new RangeError();
+    }
     var centuary= Math.ceil(year/100);
     document.write(centuary);
     };
-}
-centuryByYearProblem();
+
+
 
 /**
  * Переводит цвет из формата HEX в формат RGB
@@ -39,6 +46,17 @@ centuryByYearProblem();
  */
 function colorsProblem(hexColor) {
     // Ваше решение
+    const HexToRgb = /^#([A-Fa-f\d]{2})([A-Fa-f\d]{2})([A-Fa-f\d]{2})$/;
+  if (typeof hexColor !== 'string') {
+      throw new TypeError();
+  } else if (!HexToRgb.test(hexColor)) {
+      throw new RangeError();
+  }
+  let rgb = HexToRgb.exec(hexColor);
+  rgb[1] = parseInt(rgb[1], 16);
+  rgb[2] = parseInt(rgb[2], 16);
+  rgb[3] = parseInt(rgb[3], 16);
+  return `(${rgb[1]}, ${rgb[2]}, ${rgb[3]})`;
 }
 
 /**
@@ -50,7 +68,22 @@ function colorsProblem(hexColor) {
  */
 function fibonacciProblem(n) {
     // Ваше решение
+    if (typeof(n) !== 'number') {
+        throw new TypeError();
+    }
+    if (n < 1 || !Number.isInteger(n)) {
+        throw new RangeError();
+    }
+    let previous = 1;
+    let current = 1;
+    for (let i = 3; i <= n; i++) {
+        let temp = current;
+        current += previous;
+        previous = temp;
+    }
+    return current;
 }
+
 
 /**
  * Транспонирует матрицу
@@ -60,6 +93,19 @@ function fibonacciProblem(n) {
  */
 function matrixProblem(matrix) {
     // Ваше решение
+    if (!Array.isArray(matrix) || !matrix.every(Array.isArray || matrix.length === 0)) {
+        throw new TypeError();
+    }
+    var k = matrix.length;
+    var l = matrix[0].length;
+    var transposeMatrix = [];
+    for (var i = 0; i < l; i++) {
+        transposeMatrix[i] = [];
+        for (var j = 0; j < k; j++) {
+            transposeMatrix[i][j] = matrix[j][i];
+        }
+    }
+    return transposeMatrix;
 }
 
 /**
@@ -72,6 +118,13 @@ function matrixProblem(matrix) {
  */
 function numberSystemProblem(n, targetNs) {
     // Ваше решение
+    if (typeof n !== 'number' || typeof targetNs !== 'number' || !Number.isInteger(targetNs)) {
+        throw new TypeError();
+    }
+    if (targetNs < 2 || targetNs > 36) {
+        throw new RangeError();
+    }
+    return n.toString(targetNs);
 }
 
 /**
@@ -81,6 +134,10 @@ function numberSystemProblem(n, targetNs) {
  */
 function phoneProblem(phoneNumber) {
     // Ваше решение
+    if (typeof phoneNumber !== 'string') {
+        throw new TypeError('Не строка');
+    }
+    return /^8-800-\d{3}-\d{2}-\d{2}$/.test(phoneNumber);
 }
 
 /**
@@ -91,6 +148,14 @@ function phoneProblem(phoneNumber) {
  */
 function smilesProblem(text) {
     // Ваше решение
+    if (typeof text !== 'string') {
+        throw new TypeError('not a string');
+    }
+    var Smile = text.match(/(:-\)|\(-:)/gi);
+    if (!Smile) {
+        return 0;
+    }
+    return Smile.length;
 }
 
 /**
@@ -101,6 +166,21 @@ function smilesProblem(text) {
  */
 function ticTacToeProblem(field) {
     // Ваше решение
+    for (var i = 0; i < 3; i++) {
+        if (field[i][0] === field[i][1] && field[i][1] === field[i][2]) {
+            return field[i][0];
+        }
+        if (field[0][i] === field[1][i] && field[1][i] === field[2][i]) {
+            return field[0][i];
+        }
+    }
+    if (field[0][0] === field[1][1] && field[1][1] === field[2][2]) {
+        return field[0][0];
+    }
+    if (field[0][2] === field[1][1] && field[1][1] === field[2][0]) {
+        return field[0][2];
+    }
+    return 'draw';
 }
 
 module.exports = {
