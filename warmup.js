9a672f23a78016538f38154a5843260b7baf0d6d
artem <artem.gafetinov@gmail.com>
2018-10-18 02:17:24 +0500

    Complete!

--- warmup.js
+++ warmup.js
@@ -44,6 +44,9 @@ function colorsProblem(hexColor) {
     if (typeof(hexColor) !== 'string') {
         throw TypeError;
     }
+    if (hexColor.length > 7) {
+        throw RangeError;
+    }
     let rgb = new Array(3);
     for (let i = 0; i < 3; i++) {
         rgb[i] = parseInt(hexColor.charAt(i * 2 + 1) + hexColor.charAt(i * 2 + 2), 16);
