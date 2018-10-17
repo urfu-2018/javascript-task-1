0e2f4299f9bf39b4e0cd605297d368a25624be3b
artem <artem.gafetinov@gmail.com>
2018-10-18 02:48:58 +0500

    Complete!

--- warmup.js
+++ warmup.js
@@ -8,7 +8,7 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    if (typeof(a) !== 'number') {
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') {
         throw new TypeError();
     }
 
