b3242335feca0de369551e4b496d00a8138958a8
Unknown <valera9719@gmail.com>
2018-10-18 00:50:59 +0500

    test2

--- warmup.js
+++ warmup.js
@@ -110,6 +110,9 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
+    if (typeof(n) !== 'number' || typeof(targetNs) !== 'number' || targetNs < 2 || targetNs > 36) {
+        throw new TypeError('Type Error');
+    }
     if (targetNs < 2 && targetNs > 36) {
         throw new RangeError('Range Error');
     }
