a7be93da43c99aef3b03ab18617bda25ab38efc7
Jueblack <mrjoeblack47@gmail.com>
2018-10-17 14:30:15 +0500

    Задание(3)

--- warmup.js
+++ warmup.js
@@ -4,12 +4,10 @@
  */
 function abProblem(a, b) {
     if (Number.isInteger(a) === false) {
-        throw new TypeError('Integer error a')
-    }
+        throw new TypeError('Integer error a')}
 
     if (Number.isInteger(b) === false) {
-        throw new TypeError('integer error b')
-    }
+        throw new TypeError('integer error b')}
 
     return a + b
 }
@@ -17,11 +15,9 @@ function abProblem(a, b) {
   * */
 function centuryByYearProblem(year) {
     if (Number.isInteger(year)===false) {
-        throw new TypeError('Integer error')
-    }
+        throw new TypeError('Integer error')    }
     if (year<=0) {
-        throw new RangeError('only positive numbers')
-    }
+        throw new RangeError('only positive numbers')    }
     let roundedCentury = (year / 100);
     return Math.ceil(roundedCentury)
 }
@@ -30,35 +26,29 @@ function centuryByYearProblem(year) {
  */
 function colorsProblem(hexColor) {
     if (typeof hexColor !== 'string') {
-        throw new TypeError('цвет только строкой')
-    }
+        throw new TypeError('цвет только строкой')}
     if (hexColor.length !== 7) {
-        throw new RangeError('число выходит за допустимые пределы')
-    }
+        throw new RangeError('число выходит за допустимые пределы')    }
     if (hexColor[0] !== '#')
         throw new RangeError('# обязателен');
     let rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexColor);
         let r = parseInt(rgb[1], 16);
         let g = parseInt(rgb[2], 16);
         let b = parseInt(rgb[3], 16);
-    return '(' + r + ', ' + g + ', ' + b + ')'
-}
+    return '(' + r + ', ' + g + ', ' + b + ')'}
 /**
  * Находит n-ое число Фибоначчи
  */
 function fibonacciProblem(n) {
     if (Number.isInteger(n) === false) {
-        throw new TypeError('n-это целое число')
-    }
+        throw new TypeError('n-это целое число')}
     if (n < 0) {
-        throw new RangeError('n-положительное число')
-    }
+        throw new RangeError('n-положительное число')}
     let sums;
     if (n >= 2) {
         sums = fibonacciProblem(n - 1) + fibonacciProblem(n - 2)}
         else {
-            sums = n
-        }
+            sums = n}
 return sums
 }
 /**
@@ -67,18 +57,15 @@ return sums
  */
 function matrixProblem(matrix) {
     if (Array.isArray(matrix) === false) {
-        throw new TypeError('должен быть массив')
-    }
+        throw new TypeError('должен быть массив')}
     if (matrix.every(Array.isArray) === false) {
-        throw new TypeError('должен быть двумерный массив')
-    }
+        throw new TypeError('должен быть двумерный массив')}
 
     {
         let m = matrix.length, n = matrix[0].length, cell = [];
         for (let i = 0; i < n; i++) {
             cell[i] = [];
-            for (let j = 0; j < m; j++) cell[i][j] = matrix[j][i];
-        }
+            for (let j = 0; j < m; j++) cell[i][j] = matrix[j][i];}
         return cell;
     }
 }
@@ -86,38 +73,30 @@ function matrixProblem(matrix) {
  * */
 function numberSystemProblem(n, targetNs) {
     if (!Number.isInteger(n)) {
-        throw new TypeError('должно быть число')
-    }
+        throw new TypeError('должно быть число')}
     if (!Number.isInteger(targetNs)) {
-        throw new TypeError('Систеа счисления должна быть числом')
-    }
+        throw new TypeError('Систеа счисления должна быть числом')}
     if ((2 <= targetNs <= 36) === false) {
-        throw new RangeError('Система счисления от 2 до 36')
-    }
-    return n.toString(targetNs)
-}
+        throw new RangeError('Система счисления от 2 до 36')}
+    return n.toString(targetNs)}
 /**
  * Проверяет соответствие телефонного номера формату
  */
 function phoneProblem(phoneNumber) {
     if (typeof phoneNumber !== 'string'){
-        throw new TypeError('')
-    }
+        throw new TypeError('')}
     let pattern = /8-800-\d{3}-\d{2}-\d{2}/;
-    return pattern.test(phoneNumber)
-}
+    return pattern.test(phoneNumber)}
 /**
  * Определяет количество улыбающихся смайликов в строке
  */
 function smilesProblem(text) {
     if (typeof text !== 'string') {
-        throw new TypeError('string only')
-    }
+        throw new TypeError('string only')}
 
     let result = text.match(/\(-:|:-\)/ig);
     if (result === null){
-        throw new TypeError('Смайликов не найдено')
-    }
+        throw new TypeError('Смайликов не найдено')}
     return result.length
 }
 /**
@@ -127,25 +106,19 @@ function ticTacToeProblem(field) {
     const win = [];
     for (let i = 0; i < field.length; i++) {
         if (field[0][i] === field[1][i] && field[1][i] === field[2][i]) {
-            win.push(field[0][i]);
-        }
+            win.push(field[0][i]);}
         if (field[i][0] === field[i][1] && field[i][1] === field[i][2]) {
-            win.push(field[i][0]);
-        }
+            win.push(field[i][0]);}
     }
     if ((field[0][0] === field[1][1] && field[1][1] === field[2][2]) ||
         (field[0][2] === field[1][1] && field[1][1] === field[2][0])) {
-        win.push(field[1][1]);
-    }
+        win.push(field[1][1]);}
     if (win.includes('o') !== false) {
-        return 'o';
-    }
+        return 'o';}
     if (win.includes('x') !== false) {
-        return 'x';
-    }
+        return 'x';}
     if (win.includes('o') === false && win.includes('x') === false) {
-        return 'draw';
-    }
+        return 'draw';}
 }
 module.exports = {
     abProblem,
