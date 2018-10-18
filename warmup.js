946557e804368ed71c0c17ce95cc49902bd0afd3
g0djan <g0dj4n@gmail.com>
2018-10-19 01:01:17 +0500

    тесты глянуть

--- warmup.js
+++ warmup.js
@@ -8,7 +8,11 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    // Ваше решение
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError('a и b должны быть числами');
+    }
+
+    return a + b;
 }
 
 /**
@@ -19,7 +23,14 @@ function abProblem(a, b) {
  * @returns {Number} Век, полученный из года
  */
 function centuryByYearProblem(year) {
-    // Ваше решение
+    if (typeof year !== 'number') {
+        throw new TypeError('Год - число');
+    }
+    if (year <= 0) {
+        throw new RangeError('Год - целое положительное число');
+    }
+
+    return (year / 100) + 1;
 }
 
 /**
@@ -30,7 +41,17 @@ function centuryByYearProblem(year) {
  * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
  */
 function colorsProblem(hexColor) {
-    // Ваше решение
+    if (typeof hexColor !== 'string') {
+        throw new TypeError('Цвет - hex строка');
+    }
+    if (!RegExp.test('/#[0-9A-Fa-f]{6}/g')) {
+        throw new RangeError('Цвета выходят за пределы допустимых');
+    }
+
+    return '(' + hexColor
+        .substr(1)
+        .match(/..?/g)
+        .join(', ') + ')';
 }
 
 /**
@@ -41,7 +62,18 @@ function colorsProblem(hexColor) {
  * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
  */
 function fibonacciProblem(n) {
-    // Ваше решение
+    if (typeof n !== 'number') {
+        throw new TypeError('n - число');
+    }
+    if (n < 0) {
+        throw new RangeError('n - целое положительное число');
+    }
+    const fibNumbs = [0, 1];
+    for (let i = 2; i <= n; ++i) {
+        fibNumbs.push(fibNumbs[i - 1] + fibNumbs[i - 2]);
+    }
+
+    return fibNumbs.pop();
 }
 
 /**
@@ -51,7 +83,19 @@ function fibonacciProblem(n) {
  * @returns {(Any[])[]} Транспонированная матрица размера NxM
  */
 function matrixProblem(matrix) {
-    // Ваше решение
+    if (typeof matrix.constructor !== Array || typeof matrix[0].constructor !== Array) {
+        throw new TypeError('не двумерный массив');
+    }
+    const create = (amount) => new Array(amount).fill(0);
+    const createMatrix = (rows, cols) => create(cols).map((o, i) => create(rows));
+    const newMatrix = createMatrix(matrix[0].length, matrix.length);
+    for (let i = 0; i < matrix.length; ++i) {
+        for (let j = 0; j < matrix[i].length; ++j) {
+            newMatrix[j][i] = matrix[i][j];
+        }
+    }
+
+    return newMatrix;
 }
 
 /**
@@ -63,7 +107,14 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    // Ваше решение
+    if (typeof n !== 'number' || typeof targetNs !== 'number') {
+        throw new TypeError('На вход подаются числа');
+    }
+    if (targetNs < 2 || targetNs > 36) {
+        throw new RangeError('Очнование системы счисления - число от 2 до 36');
+    }
+
+    return toString(parseInt(toString(n), targetNs));
 }
 
 /**
@@ -72,7 +123,11 @@ function numberSystemProblem(n, targetNs) {
  * @returns {Boolean} Если соответствует формату, то true, а иначе false
  */
 function phoneProblem(phoneNumber) {
-    // Ваше решение
+    if (typeof phoneNumber !== 'string') {
+        throw new TypeError('Номер телефона строка');
+    }
+
+    return RegExp.test(phoneNumber, '/8-800-[0-9]{3}-[0-9]{2}-[0-9]{2}/g');
 }
 
 /**
@@ -82,7 +137,18 @@ function phoneProblem(phoneNumber) {
  * @returns {Number} Количество улыбающихся смайликов в строке
  */
 function smilesProblem(text) {
-    // Ваше решение
+    if (typeof text !== 'string') {
+        throw new TypeError('в качестве аргумента передаётся строка');
+    }
+    let cnt = 0;
+    for (let i = 2; i < text.length; ++i) {
+        if (text[i - 2] === '(' && text[i - 1] === '-' && text[i] === ':' ||
+            text[i - 2] === ':' && text[i - 1] === '-' && text[i] === ')') {
+            ++cnt;
+        }
+    }
+
+    return cnt;
 }
 
 /**
@@ -92,7 +158,26 @@ function smilesProblem(text) {
  * @returns {'x' | 'o' | 'draw'} Результат игры
  */
 function ticTacToeProblem(field) {
-    // Ваше решение
+    let o = false;
+    let x = false;
+    const firstDiag = threeSame({ 'x': 0, 'y': 0 }, { 'x': 1, 'y': 1 }, field);
+    const secondDiag = threeSame({ 'x': 0, 'y': 2 }, { 'x': 1, 'y': -1 }, field);
+    o = o || oneIs(firstDiag, secondDiag, 'o');
+    x = x || oneIs(firstDiag, secondDiag, 'x');
+    for (let i = 0; i < 3; ++i) {
+        const a = threeSame({ 'x': i, 'y': 0 }, { 'x': 1, 'y': 0 }, field);
+        const b = threeSame({ 'x': 0, 'y': i }, { 'x': 0, 'y': 1 }, field);
+        o = o || oneIs(a, b, 'o');
+        x = x || oneIs(a, b, 'x');
+    }
+    if (o) {
+        return 'o';
+    }
+    if (x) {
+        return 'x';
+    }
+
+    return 'draw';
 }
 
 module.exports = {
@@ -106,3 +191,25 @@ module.exports = {
     smilesProblem,
     ticTacToeProblem
 };
+
+
+function threeSame(start, shift, field) {
+    const base = field[start.x][start.y];
+    let cnt = 0;
+    for (let i = 1; i < 3; ++i) {
+        start.x += shift.x;
+        start.y += shift.y;
+        if (base === field[start.x][start.y]) {
+            ++cnt;
+        }
+    }
+    if (cnt === 3) {
+        return base;
+    }
+
+    return null;
+}
+
+function oneIs(a, b, s) {
+    return a === s || b === s;
+}
