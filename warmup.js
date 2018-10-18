264281eae73f8a01680f774bf9d4b16444cbcf4b
MPISHI <mchangakutemba.com>
2018-10-18 22:58:00 +0500

    home

--- warmup.js
+++ warmup.js
@@ -93,7 +93,8 @@ function fibonacciProblem(n) {
  */
 
 // Ваше решение
-function matrixProblem(a) {let t = [];
+function matrixProblem(a) {
+    let t = [];
     for (let i = 0; i < a.length; i++) {
 
         if (!Array.isArray(a) || a[i].length !== a[0].length) {
