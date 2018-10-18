dd910873192841d2cb0926b2a47dcc6746b8c621
Andrey Denisov <andenozr@mail.ru>
2018-10-18 23:08:11 +0500

    task 1 javascript

--- warmup.js
+++ warmup.js
@@ -84,7 +84,6 @@ function fibonacciProblem(n) {
  * @returns {(Any[])[]} Транспонированная матрица размера NxM
  */
 function matrixProblem(matrix) {
-    if (matrix.length === 1 || matrix[0])
     const rows = matrix.length;
     const columns = matrix[0].length;
     const transposed = [];
