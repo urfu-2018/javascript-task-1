5e8fb9c3149a84d24a58ed2de9a20bf16227ad8b
InDevRus <InDevRus@hotmail.com>
2018-10-19 03:23:44 +0500

    Solved

--- warmup.js
+++ warmup.js
@@ -1,5 +1,34 @@
 'use strict';
 
+function checkThat(object) {
+    function checker(predicate) {
+        if (!predicate(object)) {
+            throw new TypeError();
+        }
+    }
+
+    return {
+        hasType: representativeSupplier =>
+            checkThat(typeof object === typeof representativeSupplier()),
+
+        satisfies: checker
+    };
+}
+
+function realNumber() {
+    return 0;
+}
+
+function range(end) {
+    return Array.from(Array(end).keys());
+}
+
+function symbolRange(from, length) {
+    return range(length)
+        .map(number => from.charCodeAt(0) + number)
+        .map(characterCode => String.fromCharCode(characterCode));
+}
+
 /**
  * Складывает два целых числа
  * @param {Number} a Первое целое
@@ -8,7 +37,12 @@
  * @returns {Number} Сумма аргументов
  */
 function abProblem(a, b) {
-    // Ваше решение
+    checkThat(a)
+        .hasType(realNumber);
+    checkThat(b)
+        .hasType(realNumber);
+
+    return b + a;
 }
 
 /**
@@ -19,7 +53,13 @@ function abProblem(a, b) {
  * @returns {Number} Век, полученный из года
  */
 function centuryByYearProblem(year) {
-    // Ваше решение
+    checkThat(year)
+        .hasType(realNumber);
+    if (year < 0) {
+        throw new RangeError();
+    }
+
+    return 1 + Math.floor(year / 100);
 }
 
 /**
@@ -30,7 +70,21 @@ function centuryByYearProblem(year) {
  * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
  */
 function colorsProblem(hexColor) {
-    // Ваше решение
+    checkThat(hexColor)
+        .hasType(String);
+    const firstSymbol = '#';
+    const colorLength = firstSymbol.length + 6;
+    if (hexColor.charAt(0) !== firstSymbol || hexColor.length !== colorLength) {
+        throw new RangeError();
+    }
+
+    const tokensIndexes = [1, 3, 5];
+    const answer = tokensIndexes.map(index => hexColor.substr(index, 2))
+        .map(hex => parseInt(hex, 16))
+        .map(String)
+        .join(', ');
+
+    return `(${answer})`;
 }
 
 /**
@@ -41,9 +95,30 @@ function colorsProblem(hexColor) {
  * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
  */
 function fibonacciProblem(n) {
-    // Ваше решение
+    checkThat(n)
+        .hasType(realNumber);
+    if (n < 1 || Math.trunc(n) !== n) {
+        throw new RangeError();
+    }
+
+    const fibonacciIndex = n - 1;
+    const base = [1, 1];
+
+    function fibonacciRecurrence(unused, index, recurrence) {
+        if (index < base.length) {
+            return base[index];
+        }
+
+        return recurrence[index - 1] + recurrence[index - 2];
+    }
+
+    return base
+        .concat(Array(Math.max(0, fibonacciIndex - 2)))
+        .map(fibonacciRecurrence)
+        .pop();
 }
 
+// noinspection JSValidateJSDoc
 /**
  * Транспонирует матрицу
  * @param {(Any[])[]} matrix Матрица размерности MxN
@@ -51,7 +126,28 @@ function fibonacciProblem(n) {
  * @returns {(Any[])[]} Транспонированная матрица размера NxM
  */
 function matrixProblem(matrix) {
-    // Ваше решение
+    checkThat(matrix)
+        .satisfies(Array.isArray);
+
+    const answer = [];
+
+    function writeRowToColumn(rowNumber) {
+        const row = matrix[rowNumber];
+        checkThat(row)
+            .satisfies(Array.isArray);
+        for (let columnNumber = 0; columnNumber < row.length; columnNumber++) {
+            if (rowNumber === 0) {
+                answer[columnNumber] = [];
+            }
+            answer[columnNumber][rowNumber] = row[columnNumber];
+        }
+    }
+
+    for (let rowNumber = 0; rowNumber < matrix.length; rowNumber++) {
+        writeRowToColumn(rowNumber);
+    }
+
+    return answer;
 }
 
 /**
@@ -63,7 +159,16 @@ function matrixProblem(matrix) {
  * @returns {String} Число n в системе счисления targetNs
  */
 function numberSystemProblem(n, targetNs) {
-    // Ваше решение
+    checkThat(n)
+        .hasType(realNumber);
+    checkThat(targetNs)
+        .hasType(realNumber);
+
+    if (targetNs < 2 || targetNs > 36) {
+        throw new RangeError();
+    }
+
+    return n.toString(targetNs);
 }
 
 /**
@@ -72,7 +177,28 @@ function numberSystemProblem(n, targetNs) {
  * @returns {Boolean} Если соответствует формату, то true, а иначе false
  */
 function phoneProblem(phoneNumber) {
-    // Ваше решение
+    checkThat(phoneNumber)
+        .hasType(String);
+    const eightIndexes = [0, 2];
+    const zeroIndexes = [3, 4];
+    const dashIndexes = [1, 5, 9, 12];
+    const anyDigitIndexes = [6, 7, 8, 10, 11, 13, 14];
+    const expectedLength = 15;
+
+    const equals = object => other => other === object;
+
+    const associativity = [
+        [eightIndexes, equals('8')],
+        [zeroIndexes, equals('0')],
+        [dashIndexes, equals('-')],
+        [anyDigitIndexes, symbol => symbolRange('0', 10)
+            .includes(symbol)]
+    ];
+
+    return phoneNumber.length === expectedLength &&
+        associativity.every(pair => pair[0].map(i => phoneNumber.charAt(i))
+            .every(pair[1]));
+
 }
 
 /**
@@ -82,7 +208,21 @@ function phoneProblem(phoneNumber) {
  * @returns {Number} Количество улыбающихся смайликов в строке
  */
 function smilesProblem(text) {
-    // Ваше решение
+    checkThat(text).hasType(String);
+
+    const smile = '(-:';
+    const reversedSmile = ':-)';
+    let index = 0;
+    let count = 0;
+
+    do {
+        let newIndex = text.indexOf(smile, index);
+        index = newIndex !== -1 ? newIndex : text.indexOf(reversedSmile, index);
+        count += index === -1 ? 0 : 1;
+        index += index >= 0 ? 1 : 0;
+    } while (index >= 0);
+
+    return count;
 }
 
 /**
@@ -92,7 +232,66 @@ function smilesProblem(text) {
  * @returns {'x' | 'o' | 'draw'} Результат игры
  */
 function ticTacToeProblem(field) {
-    // Ваше решение
+    function getAssociations() {
+        const moveHorizontal = pair => [pair[0] + 1, pair[1]];
+        const moveVertical = pair => [pair[0], pair[1] + 1];
+        const moveDiagonal = pair => [pair[0] + 1, pair[1] + 1];
+        const moveBackDiagonal = pair => [pair[0] - 1, pair[1] + 1];
+
+        const horizontalStarts = [[0, 0], [0, 1], [0, 2]];
+        const verticalStarts = [[0, 0], [1, 0], [2, 0]];
+        const diagonalStarts = [[0, 0]];
+        const backDiagonalStarts = [[2, 0]];
+
+        return [
+            [horizontalStarts, moveHorizontal],
+            [verticalStarts, moveVertical],
+            [diagonalStarts, moveDiagonal],
+            [backDiagonalStarts, moveBackDiagonal]
+        ];
+    }
+
+    const limit = 3;
+
+    function makeRoad(start, incrementerFunction) {
+        let current = start;
+        const answer = [];
+
+        while (answer.length < limit) {
+            answer.push(current);
+            current = incrementerFunction(current);
+        }
+
+        return answer;
+    }
+
+    const getByIndex = (x, y) => field[x][y];
+
+    function uniqueValue(values) {
+        if (values.includes('x') && !values.includes('o')) {
+            return 'x';
+        }
+        if (values.includes('o') && !values.includes('x')) {
+            return 'o';
+        }
+    }
+
+    function roadToUniqueValue(road) {
+        const values = road.map(coordinate => getByIndex(...coordinate));
+
+        return uniqueValue(values);
+    }
+
+    const productStartsToIncrementer = (starts, incrementer) =>
+        starts.map(coordinate => [coordinate, incrementer]);
+
+    const winners = getAssociations()
+        .map(startsAndIncrementer => productStartsToIncrementer(...startsAndIncrementer))
+        .reduce((first, second) => first.concat(second))
+        .map(startAndIncrementer => makeRoad(...startAndIncrementer))
+        .map(roadToUniqueValue);
+
+    return uniqueValue(winners);
 }
 
 module.exports = {
