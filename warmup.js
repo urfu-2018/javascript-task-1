8c77e05873be5377f5617f1f28cb97daea8ac662
sofelvpopel <rigelshrigel@gmail.com>
2018-10-19 00:34:55 +0500

    h

--- warmup.js
+++ warmup.js
@@ -160,7 +160,7 @@ function numberSystemProblem(n, targetNs) {
     }
 
     const abc = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
-        'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'Q',
+        'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
         'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
     let result = '';
