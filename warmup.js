407869bc1d4ea1c0f31f938c92711107d1f61635
Jueblack <mrjoeblack47@gmail.com>
2018-10-19 00:28:40 +0500

    исправил phoneProblem

--- warmup.js
+++ warmup.js
@@ -102,7 +102,7 @@ function phoneProblem(phoneNumber) {
     if (typeof phoneNumber !== 'string') {
         throw new TypeError('Номер строкой');
     }
-    let pattern = /8-800-\d{3}-\d{2}-\d{2}$/;
+    let pattern = /^8-800-\d{3}-\d{2}-\d{2}$/;
 
     return pattern.test(phoneNumber);
 }
