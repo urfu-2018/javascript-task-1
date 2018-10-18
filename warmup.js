c4aa4f0fbed8dbf76d3445e1c6721053a842ba5a
Anton Mukhinsky <antonn.aka.fly@gmail.com>
2018-10-18 13:15:08 +0500

    вернул проверку массива

--- warmup.js
+++ warmup.js
@@ -91,6 +91,9 @@ function fibonacciProblem(n) {
  * @returns {(Any[])[]} Транспонированная матрица размера NxM
  */
 function matrixProblem(matrix) {
+    if (!Array.isArray(matrix)) {
+        throw new TypeError('передан неверный тип данных');
+    }
     for (let i = 0; i < matrix.length; i++) {
         if (!Array.isArray(matrix[i]) || matrix[i].length !== matrix[0].length) {
             throw new TypeError('передан неверный тип данных');
