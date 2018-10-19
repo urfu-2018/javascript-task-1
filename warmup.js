591af390239ce53d841e01f776d4c9e8260f3c72
Maxim <ya.max-m2013@yandex.ru>
2018-10-19 15:26:11 +0500

    sfsd

--- warmup.js
+++ warmup.js
@@ -157,20 +157,21 @@ function smilesProblem(text) {
  * @returns {'x' | 'o' | 'draw'} Результат игры
  */
 function ticTacToeProblem(field) {
-    if ((field[0][0] === field[1][1] && field[1][1] === field[2][2]) ||
-        (field[0][2] === field[1][1] && field[1][1] === field[2][0])) {
-        return field[1][1];
-    }
     for (let i = 0; i < 3; i++) {
-        if (field[i][0] === field[i][1] && field[i][1] === field[i][2]) {
-            return field[i][0];
-        }
         if (field[0][i] === field[1][i] && field[1][i] === field[2][i]) {
             return field[0][i];
         }
+        if (field[i][0] === field[i][1] && field[i][1] === field[i][2]) {
+            return field[i][0];
+        }
     }
-
-    return 'draw';
+    if (field[0][0] === field[1][1] && field[1][1] === field[2][2]) {
+        return field[0][0];
+    }
+    if (field[0][2] === field[1][1] && field[1][1] === field[2][0]) {
+        return field[0][2];
+    }
+     return 'draw';
 }
 
 
