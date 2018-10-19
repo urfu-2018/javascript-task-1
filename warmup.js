9fbb313ed962f1e401de43c37eb069dcf40878a6
Дразнина Наталья <natalya_draznina@mail.ru>
2018-10-19 13:34:41 +0500

    а теперь???

--- warmup.js
+++ warmup.js
@@ -129,7 +129,7 @@ function phoneProblem(phoneNumber) {
         throw new TypeError();
     }
 
-    return /^8-800-\d\d\d-\d\d-\d\d$/.test(phoneNumber);
+    return /^8-800-[\d]{3}-[\d]{2}-[\d]{2}$/.test(phoneNumber);
 }
 
 /**
