876de0690498b4be894b84f951dc9f848e14974d
Vasya <perminov.vasya@mail.ru>
2018-10-23 10:13:28 +0500

    test

--- warmup.js
+++ warmup.js
@@ -7,7 +7,7 @@
  * @throws {TypeError} Когда в аргументы переданы не числа
  * @returns {Number} Сумма аргументов
  */
- function abProblem(a, b) {
+function abProblem(a, b) {
    if((isNaN(parseFloat(a)) && isFinite(a))||(isNaN(parseFloat(b)) && isFinite(b)))
       throw new TypeError();
   return a+b;
@@ -22,11 +22,17 @@
  */
  function centuryByYearProblem(year) {
    if(isNaN(parseFloat(year)) && isFinite(year))
+   {
       throw new TypeError();
+   }
   if(year<0)
+  {
      throw new  RangeError();
+  }
   if(year%100 == 0)
+  {
       return year/100;
+  }
   else return year/100 + 1;
 }
 
@@ -39,12 +45,16 @@
  */
  function colorsProblem(hexColor) {
     if(typeof hexColor !== 'string')
+    {
     	throw new TypeError();
+    }
     var R = hexColor.substring(1, 3);
     var G = hexColor.substring(3, 5);
     var B = hexColor.substring(5, 7);
     if(R > 'FF' || G > 'FF' || B > 'FF')
+    {
     	throw new  RangeError();
+    }
     return '('+parseInt(R, 16)+', '+parseInt(G, 16)+', '+parseInt(B, 16)+')';
 }
 
@@ -57,9 +67,12 @@
  */
  function fibonacciProblem(n) {
     if(isNaN(parseFloat(n)) && isFinite(n))
+    {
     	throw new TypeError();
-    if (n <= 0 || !Number.isInteger(n))
+    }
+    if (n <= 0 || !Number.isInteger(n)){
     	throw new  RangeError();
+    }
     var f = 1;
     var s = 1;
     for (var i = 1; i <= n; i++) {
@@ -76,7 +89,9 @@
  */
  function matrixProblem(matrix) {
     if(!matrix.every(Array.isArray)||!Array.isArray(matrix))
+    {
     	throw new TypeError();
+    }
     let tempMatr = matrix;
     for (i = 0; i < matrix.length; i++) {
         for (j = 0; j < matrix.length; j++){
@@ -96,9 +111,12 @@
  */
  function numberSystemProblem(n, targetNs) {
    if(isNaN(parseFloat(year)) && isFinite(year))
+   {
       throw new TypeError();
-  if (targetNs<=2 || targetNs>=36) 
+   }
+  if (targetNs<=2 || targetNs>=36) {
       throw new  RangeError();
+  }
   return parseInt(n.toString, targetNs);
 }
 
@@ -120,7 +138,9 @@
  */
  function smilesProblem(text) {
      if(typeof(text)!=='string')
+     {
         throw new TypeError();
+    }
     var count = 0 ;
     for (var i = 0; i < text.length; i++)
     {
@@ -138,21 +158,28 @@
  * @returns {'x' | 'o' | 'draw'} Результат игры
  */
  function ticTacToeProblem(field) {
-   if (!Array.isArray(field) || field.length !== 3) 
+   if (!Array.isArray(field) || field.length !== 3)
+   {
     throw new TypeError();
+    }
 
-if (field[0][0] === field[1][1] && field[1][1] === field[2][2]) 
+if (field[0][0] === field[1][1] && field[1][1] === field[2][2])
+{
     return field[0][0];
+}
 
-if (field[0][2] === field[1][1] && field[1][1] === field[2][0]) 
+if (field[0][2] === field[1][1] && field[1][1] === field[2][0]) {
     return field[0][2];
+}
 
 for (var i = 0; i < 3; i++) {
-    if (field[i][0] === field[i][1] && field[i][1] === field[i][2]) 
-        return field[i][0];
+    if (field[i][0] === field[i][1] && field[i][1] === field[i][2]) {
+            return field[i][0];
+        }
     
-    if (field[0][i] === field[1][i] && field[1][i] === field[2][i]) 
+    if (field[0][i] === field[1][i] && field[1][i] === field[2][i]) {
         return field[0][i];
+    }
     
 }
 return 'draw';
